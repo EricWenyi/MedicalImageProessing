3957510bb7b5f704d3e18c7c6bab1f69e1f88e7f
LuJia <LuJia@UESTC.org>
2015-11-02 13:11:41 +0800

    extract the mask image and perform medianFilter to the original image

---
+++ GDCMImageMedianFilter3DTo3D/GDCMImageMedianFilter3DTo3D.cxx
@@ -0,0 +1,207 @@
+//文件读写的头文件
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+//GDCM类型文件读写的头文件
+#include "itkGDCMImageIO.h"
+#include "itkGDCMSeriesFileNames.h"
+
+//切片迭代器的头文件
+#include "itkImageSliceConstIteratorWithIndex.h"
+
+//提取切片的头文件
+#include "itkExtractImageFilter.h"
+
+//切片文件集合成3D图像的头文件
+#include "itkJoinseriesImageFilter.h"
+
+//中值滤波器的头文件
+#include "itkMedianImageFilter.h"
+
+int main( int argc, char * argv[] ){
+
+	//判断参数个数，若个数不够提示需要的参数
+	if( argc < 3 ){
+		std::cerr << "Usage: " << std::endl;
+		std::cerr << argv[0] << "inputImageFile outputImageFile" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	//指定输入输出像素点类型
+	typedef signed short PixelType;
+
+	//指定输入输出的图像类型
+	typedef itk::Image< PixelType, 3 > ImageType3D;
+	typedef itk::Image< PixelType, 2 > ImageType2D;
+
+	//设定输入图像的类型
+	typedef itk::GDCMImageIO ImageIOType;
+	ImageIOType::Pointer gdcmIO = ImageIOType::New();
+
+	//设定读取器类型及设定读取的图像类型和图像地址
+	typedef itk::ImageFileReader< ImageType3D > ReaderType;
+	ReaderType::Pointer maskReader = ReaderType::New();
+	ReaderType::Pointer originReader = ReaderType::New();
+
+	maskReader->SetImageIO( gdcmIO );
+	maskReader->SetFileName( argv[1] );
+
+	originReader->SetImageIO( gdcmIO );
+	originReader->SetFileName( argv[2] );
+
+	//更新读取器，并捕获异常
+	try{
+		maskReader->Update();
+	} catch ( itk::ExceptionObject & err ){
+		std::cerr << "ExceptionObject maskReader->Update() caught !" << std::endl;
+		std::cerr << err << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	try{
+		originReader->Update();
+	} catch ( itk::ExceptionObject & err ){
+		std::cerr << "ExceptionObject originReader->Update() caught !" << std::endl;
+		std::cerr << err << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	//设定输入区域大小
+	ImageType3D::ConstPointer maskImage3D = maskReader->GetOutput();
+	ImageType3D::RegionType maskRegion3D = maskImage3D->GetLargestPossibleRegion();
+
+	ImageType3D::ConstPointer originImage3D = originReader->GetOutput();
+	ImageType3D::RegionType originRegion3D = originImage3D->GetLargestPossibleRegion();
+	
+	//设定切片迭代器
+	typedef itk::ImageSliceConstIteratorWithIndex < ImageType3D > SliceIteratorType;
+	SliceIteratorType maskIterator( maskImage3D, maskRegion3D );
+	maskIterator.SetFirstDirection(0);
+	maskIterator.SetSecondDirection(1);
+
+	SliceIteratorType originIterator( originImage3D, originRegion3D );
+	originIterator.SetFirstDirection(0);
+	originIterator.SetSecondDirection(1);
+
+	//设置图片组合器
+	typedef itk::JoinSeriesImageFilter< ImageType2D, ImageType3D > JoinSeriesFilterType;
+	JoinSeriesFilterType::Pointer joinSeries = JoinSeriesFilterType::New();
+	joinSeries->SetOrigin(originImage3D->GetOrigin()[2]);
+	joinSeries->SetSpacing(originImage3D->GetSpacing()[2]);
+	
+	for(maskIterator.GoToBegin(), originIterator.GoToBegin(); !maskIterator.IsAtEnd() || !originIterator.IsAtEnd(); maskIterator.NextSlice(), originIterator.NextSlice()){
+		//获取切片序号
+		ImageType3D::IndexType maskSliceIndex = maskIterator.GetIndex();
+
+		ImageType3D::IndexType originSliceIndex = originIterator.GetIndex();
+
+		//获取每张切片的大小，并设置每张切片的Z轴为0
+		typedef itk::ExtractImageFilter< ImageType3D, ImageType2D > ExtractFilterType;
+		ExtractFilterType::InputImageRegionType maskSliceRegion = maskIterator.GetRegion();
+		ExtractFilterType::InputImageRegionType::SizeType maskSliceSize = maskSliceRegion.GetSize();
+		maskSliceSize[2] = 0;
+
+		ExtractFilterType::InputImageRegionType originSliceRegion = originIterator.GetRegion();
+		ExtractFilterType::InputImageRegionType::SizeType originSliceSize = originSliceRegion.GetSize();
+		originSliceSize[2] = 0;
+
+		//设定每张切片的大小和序号
+		maskSliceRegion.SetSize( maskSliceSize );
+		maskSliceRegion.SetIndex( maskSliceIndex );
+
+		originSliceRegion.SetSize( originSliceSize );
+		originSliceRegion.SetIndex( originSliceIndex );
+		
+		//指定过滤器类型
+		ExtractFilterType::Pointer maskExtractFilter = ExtractFilterType::New();
+		maskExtractFilter->SetExtractionRegion( maskSliceRegion );
+
+		ExtractFilterType::Pointer originExtractFilter = ExtractFilterType::New();
+		originExtractFilter->SetExtractionRegion( originSliceRegion );
+		
+		//到位并开启过滤器，修改方向坍塌策略为子矩阵
+		maskExtractFilter->InPlaceOn();
+		maskExtractFilter->SetDirectionCollapseToSubmatrix();
+		maskExtractFilter->SetInput( maskReader->GetOutput() );
+		
+		originExtractFilter->InPlaceOn();
+		originExtractFilter->SetDirectionCollapseToSubmatrix();
+		originExtractFilter->SetInput( originReader->GetOutput() );
+		
+		//更新过滤器，并捕获异常
+		try{
+			maskExtractFilter->Update();
+		} catch (itk::ExceptionObject &excp){
+			std::cerr << "ExceptionObject: maskExtractFilter->Update() caught !" << std::endl;
+			std::cerr << excp << std::endl;
+			return EXIT_FAILURE;
+		}
+		
+		try{
+			originExtractFilter->Update();
+		} catch (itk::ExceptionObject &excp){
+			std::cerr << "ExceptionObject: originExtractFilter->Update() caught !" << std::endl;
+			std::cerr << excp << std::endl;
+			return EXIT_FAILURE;
+		}
+
+		//掩模提取
+		typedef itk::ImageRegionConstIterator< ImageType2D > ConstIteratorType;
+		typedef itk::ImageRegionIterator< ImageType2D > IteratorType;
+
+		ConstIteratorType mask( maskExtractFilter->GetOutput(), maskExtractFilter->GetOutput()->GetRequestedRegion() );
+		IteratorType origin( originExtractFilter->GetOutput(), originExtractFilter->GetOutput()->GetRequestedRegion() );
+		for ( mask.GoToBegin(), origin.GoToBegin(); !mask.IsAtEnd() || !origin.IsAtEnd(); mask++, origin++ ){
+			if( mask.Get() == 0 );
+			else origin.Set( 0 );
+		}
+
+		//设定原图的中值滤波器的输入输出图像类型
+		typedef itk::MedianImageFilter< ImageType2D, ImageType2D >  MedianFilterType;
+		MedianFilterType::Pointer medianFilter = MedianFilterType::New();
+
+		//设定水平半径与竖直半径
+		ImageType2D::SizeType indexRadius;
+		indexRadius.Fill(2);
+		medianFilter->SetRadius( indexRadius );
+
+		//设定中值滤波器输入
+		medianFilter->SetInput( originExtractFilter->GetOutput() );
+		
+		//更新中值滤波器，并捕获异常
+		try{
+			medianFilter->Update();
+		} catch ( itk::ExceptionObject & err ){
+			std::cerr << "ExceptionObject medianFilter->Update() caught !" << std::endl;
+			std::cerr << err << std::endl;
+			return EXIT_FAILURE;
+		}
+		
+		joinSeries->PushBackInput( medianFilter->GetOutput() );
+	}
+	
+	try{
+		joinSeries->Update();
+	} catch ( itk::ExceptionObject & err ){
+		std::cerr << "ExceptionObject joinSeries->Update() caught !" << std::endl;
+		std::cerr << err << std::endl;
+		return EXIT_FAILURE;
+	}
+	
+	//设定写入器输入
+	typedef itk::ImageFileWriter< ImageType3D > WriterType;
+	WriterType::Pointer writer = WriterType::New();
+	writer->SetInput( joinSeries->GetOutput() );
+	writer->SetFileName( argv[3] );	
+	
+	//更新写入器，并捕获异常
+	try{
+		writer->Update();
+	} catch ( itk::ExceptionObject & err ){
+		std::cerr << "ExceptionObject writer->Update() caught !" << std::endl;
+		std::cerr << err << std::endl;
+		return EXIT_FAILURE;
+	}
+	
+	return EXIT_SUCCESS;
+}
