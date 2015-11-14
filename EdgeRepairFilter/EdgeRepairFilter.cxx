56e156267d99202995f1c5c5eedf63387531a200
bowringchan <bowringchan@gmail.com>
2015-11-14 21:42:04 +0800

    Connected with OpenCV. Extract Vector of contours.

--- EdgeRepairFilter/EdgeRepairFilter.cxx
+++ EdgeRepairFilter/EdgeRepairFilter.cxx
@@ -16,10 +16,14 @@
 
 #include "itkRescaleIntensityImageFilter.h"
 
+#include "itkOpenCVImageBridge.h"
+
+// includes from OpenCV
+#include "cv.h"
 
 int main( int argc, char* argv[] ){
-	if( argc < 7 ){
-		std::cerr << "Usage: " << argv[0] << " InputImageFile OutputImageFile Sigma SigmoidAlpha SigmoidBeta TimeThreshold" << std::endl;
+	if( argc < 3 ){
+		std::cerr << "Usage: " << argv[0] << " InputImageFile OutputImageFile" << std::endl;
 		return EXIT_FAILURE;
     }
 	//三维图像类型
@@ -29,7 +33,7 @@ int main( int argc, char* argv[] ){
 	
 	//二维图像类型
 	const unsigned int Dimension2D = 2;
-	typedef float PixelType2D;//此处原为signed short，应设置为float类型，下文CurvatureFlowImageFilter所要求，否则将会弹窗报错至失去响应
+	typedef unsigned char PixelType2D;//此处原为signed short，应设置为float类型，下文CurvatureFlowImageFilter所要求，否则将会弹窗报错至失去响应
 	typedef itk::Image< PixelType2D, Dimension2D > ImageType2D;
 
 
@@ -91,64 +95,33 @@ int main( int argc, char* argv[] ){
 			return EXIT_FAILURE;
 		}
 		
-		//二值滤波器
-		typedef itk::BinaryThresholdImageFilter< ImageType2D,
-                        ImageType2D    >    ThresholdingFilterType;
-		ThresholdingFilterType::Pointer thresholder = ThresholdingFilterType::New();
-		const PixelType2D  timeThreshold = atof( argv[6] );
-
-		//因为Sigmoid函数输出是0到1之间的值，故取阀值0.5
-		thresholder->SetLowerThreshold(0.5);
-		thresholder->SetUpperThreshold( timeThreshold  );//timeThreshold输入为1即可
-
-		thresholder->SetOutsideValue(  0  );
-		thresholder->SetInsideValue(  255 );
-		//以下过程的流程图参见ITK II P386页，得到Edge Image
-		typedef itk::RescaleIntensityImageFilter<
-                               ImageType2D,
-                               ImageType2D >   CastFilterType;
-		typedef   itk::CurvatureAnisotropicDiffusionImageFilter<
-                               ImageType2D,
-                               ImageType2D >  SmoothingFilterType;
-		SmoothingFilterType::Pointer smoothing = SmoothingFilterType::New();
-		typedef   itk::GradientMagnitudeRecursiveGaussianImageFilter<
-                               ImageType2D,
-                               ImageType2D >  GradientFilterType;
-		typedef   itk::SigmoidImageFilter<
-                               ImageType2D,
-                               ImageType2D >  SigmoidFilterType;
-		GradientFilterType::Pointer  gradientMagnitude = GradientFilterType::New();
-		SigmoidFilterType::Pointer sigmoid = SigmoidFilterType::New();
-		sigmoid->SetOutputMinimum(  0.0  );
-		sigmoid->SetOutputMaximum(  1  );
-		typedef  itk::FastMarchingImageFilter< ImageType2D,
-                              ImageType2D >    FastMarchingFilterType;
-		FastMarchingFilterType::Pointer  fastMarching
-                                              = FastMarchingFilterType::New();
-		smoothing->SetTimeStep( 0.1 );
-		smoothing->SetNumberOfIterations(  5 );
-		smoothing->SetConductanceParameter( 9.0 );
-		const double sigma = atof( argv[3] );
-		gradientMagnitude->SetSigma(  sigma  );
-		const double alpha =  atof( argv[4] );
-		const double beta  =  atof( argv[5] );
-
-		sigmoid->SetAlpha( alpha );
-		sigmoid->SetBeta(  beta  );
+		using namespace cv;
+		Mat img = itk::OpenCVImageBridge::ITKImageToCVMat< ImageType2D >( inExtractor->GetOutput() );
+		vector<vector<Point>> contours;
+		vector<Vec4i> hierarchy;
+		RNG rng(12345);
+  /// Find contours
+		findContours( img, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
 
-		smoothing->SetInput( inExtractor->GetOutput() );
-		gradientMagnitude->SetInput( smoothing->GetOutput() );
-		sigmoid->SetInput( gradientMagnitude->GetOutput() );
-		thresholder->SetInput( sigmoid->GetOutput() );
+
+  /// Draw contours
+		Mat drawing = Mat::zeros( img.size(), CV_8UC3 );
+		for( int i = 0; i< contours.size(); i++ )
+		{
+			 Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+			drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+		}
+
+		ImageType2D::Pointer itkDrawing;
 		try{
-			thresholder->Update();
-		}catch (itk::ExceptionObject &excp){
-		std::cerr << "ExceptionObject: Thresholder->Update(); caught !" << std::endl;
-		std::cerr << excp << std::endl;
-		return EXIT_FAILURE;
-  }
-  joinSeries->PushBackInput( thresholder->GetOutput() );
+		itkDrawing=itk::OpenCVImageBridge::CVMatToITKImage<ImageType2D>(drawing);
+		} catch (itk::ExceptionObject &excp){
+			std::cerr << "Exception: CVtoITKImage failure !" << std::endl;
+			std::cerr << excp << std::endl;
+			return EXIT_FAILURE;
+		}
+		joinSeries->PushBackInput(itkDrawing );
 }
 
   
