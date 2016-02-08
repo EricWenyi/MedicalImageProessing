6482759efa8a34fb345a19d38bfb3249da431bc1
LuJia <LuJia@UESTC.org>
2016-02-08 11:12:58 +0800

    just label the points of 3D image, no drawing

---
+++ GDCMLabelingImage3D/GDCMLabelingImage3D.cxx
@@ -0,0 +1,267 @@
+#include "itkGDCMImageIO.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+#include "itkImageSliceConstIteratorWithIndex.h"
+#include "itkExtractImageFilter.h"
+#include "itkJoinseriesImageFilter.h"
+#include "itkConstNeighborhoodIterator.h"
+#include "itkOpenCVImageBridge.h"
+
+int main( int argc, char* argv[] ){
+	if( argc < 3 ){
+		std::cerr << "Usage: " << argv[0] << "InputImageFile OutputImageFile" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	typedef signed short PixelType3D;
+	typedef itk::Image< PixelType3D, 3 > ImageType3D;
+
+	typedef unsigned char PixelType2D;
+	typedef itk::Image< PixelType2D, 2 > ImageType2D;
+
+	typedef itk::GDCMImageIO ImageIOType;
+	ImageIOType::Pointer gdcmIO = ImageIOType::New();
+
+	typedef itk::ImageFileReader< ImageType3D > ReaderType;
+	ReaderType::Pointer originReader = ReaderType::New();
+	originReader->SetImageIO( gdcmIO );
+	originReader->SetFileName( argv[1] );
+
+	try{
+		originReader->Update();
+	} catch (itk::ExceptionObject &excp){
+		std::cerr << "ExceptionObject: reader->Update() caught !" << std::endl;
+		std::cerr << excp << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	ImageType3D::ConstPointer originImage3D = originReader->GetOutput();
+	typedef itk::ImageSliceConstIteratorWithIndex < ImageType3D > SliceIteratorType;
+
+	SliceIteratorType inIterator( originImage3D, originImage3D->GetLargestPossibleRegion() );
+	inIterator.SetFirstDirection( 0 );
+	inIterator.SetSecondDirection( 1 );
+
+	typedef itk::ExtractImageFilter< ImageType3D, ImageType2D > ExtractFilterType;
+	typedef itk::JoinSeriesImageFilter< ImageType2D, ImageType3D > JoinSeriesFilterType;
+
+	JoinSeriesFilterType::Pointer joinSeries = JoinSeriesFilterType::New();
+	joinSeries->SetOrigin( originImage3D->GetOrigin()[2] );
+	joinSeries->SetSpacing( originImage3D->GetSpacing()[2] );\
+
+	using namespace cv;
+
+	struct APoint{
+		int x;
+		int y;
+		int label;
+	};
+	APoint apoint;
+	vector<APoint> temp1;
+	vector<APoint> temp2;
+	vector<vector<APoint>> points;
+	
+	int labelCounter = 0;
+	int zeroCounter = 0;
+
+	//邻域迭代器
+	typedef itk::ConstNeighborhoodIterator< ImageType3D > NeighborhoodIteratorType;
+	NeighborhoodIteratorType::RadiusType radius;
+	radius.Fill(1);//设定领域半径，所有方向半径都为1，及3*3*3
+	NeighborhoodIteratorType it( radius, originImage3D, originImage3D->GetLargestPossibleRegion() );
+	ImageType3D::IndexType location;//location用于跳转邻域迭代器至所需要的点的位置
+	
+	/*
+	struct APoint{
+		int x;
+		int y;
+		int label;
+	};
+	APoint apoint;
+	vector<APoint> temp1;
+	vector<APoint> temp2;
+	vector<vector<APoint>> points;
+	
+	int labelCounter = 0;
+	int zeroCounter = 0;
+
+	//仅第一张切片中，按contours顺序推至temp1中，不同contours分配不同label，然后推至points
+	for(int i = 0; i < contours.size(); i++){
+		for(int j = 0; j < contours[i].size(); j++){
+			apoint.x = contours[i][j].x;
+			apoint.y = contours[i][j].y;
+			apoint.label = labelCounter;
+			labelCounter++;
+			temp1.push_back(apoint);
+		}
+	}
+
+	points.push_back(temp1);
+
+	//第二张切片开始，每张切片先把当前层所有contours推至temp2中去，默认所有label为-1
+	//向上一层的9个邻域搜索非0点，若一个都没有，则为新label，则当前位置label = labelCounter; labelCounter++;
+	//若有，则到上一层的temp1中搜索（根据X，Y坐标），找到邻域的点的label并把当前位置也标为这个label
+	//然后将处理好的temp2推至points
+
+	for(int i = 0; i < contours.size(); i++){
+		for(int j = 0; j < contours[i].size(); j++){
+			apoint.x = contours[i][j].x;
+			apoint.y = contours[i][j].y;
+			apoint.label = -1;
+			temp2.push_back(apoint);
+		}
+	}
+	
+	for(int i = 0; i < temp2.size(); i++){
+		location[0] = temp2[i].x;
+		location[1] = temp2[i].y;
+		it.SetLocation(location);
+		for(int j = 0; j < 9; j++){
+			if(it.GetPixel(j) != 0){
+				for(int k = 0; k < temp1.size(); k++){
+					if(temp1[k].x == it.GetIndex(j)[0] && temp1[k].y == it.GetIndex(j)[1]){
+						temp2[i].label = temp1[k].label;
+					}
+				}
+			} else {
+				zeroCounter++;
+			}
+		}
+		if(zeroCounter == 9){
+			temp2[i].label = labelCounter;
+			labelCounter++;
+			zeroCounter = 0;
+		}
+	}
+
+	points.push_back(temp2);
+
+	//然后清除temp1，并把temp2的内存复制到temp1，清除temp2
+	temp1.clear();
+	temp1.resize(temp2.size());
+	memcpy(&temp1[0], &temp2[0], temp2.size() * sizeof(APoint));
+	temp2.clear();
+
+	//后面的切片以此类推，算法结束
+	*/
+
+	for( inIterator.GoToBegin(); !inIterator.IsAtEnd(); inIterator.NextSlice() ){
+		ImageType3D::IndexType sliceIndex = inIterator.GetIndex();
+		location[2] = sliceIndex[2];//设定location的Z坐标为当前切片序号
+		printf( "Slice Index --- %d ---\n", sliceIndex[2] );
+		ExtractFilterType::InputImageRegionType::SizeType sliceSize = inIterator.GetRegion().GetSize();
+		sliceSize[2] = 0;
+		
+		ExtractFilterType::InputImageRegionType sliceRegion = inIterator.GetRegion();
+		sliceRegion.SetSize( sliceSize );
+		sliceRegion.SetIndex( sliceIndex );
+
+		ExtractFilterType::Pointer inExtractor = ExtractFilterType::New();
+		inExtractor->SetExtractionRegion( sliceRegion );
+		inExtractor->InPlaceOn();
+		inExtractor->SetDirectionCollapseToSubmatrix();
+		inExtractor->SetInput( originReader->GetOutput() );
+
+		try{
+			inExtractor->Update();
+		} catch (itk::ExceptionObject &excp){
+			std::cerr << "ExceptionObject: inExtractor->Update() caught !" << std::endl;
+			std::cerr << excp << std::endl;
+			return EXIT_FAILURE;
+		}
+
+		Mat img = itk::OpenCVImageBridge::ITKImageToCVMat< ImageType2D >( inExtractor->GetOutput() );
+
+		vector<vector<Point>> contours;
+		vector<Vec4i> hierarchy;
+		findContours( img, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_NONE, Point(0, 0) );
+		
+		Mat drawing = Mat::zeros( img.size(), CV_8UC1 );
+		
+		if(sliceIndex[2] == 0){
+			for(int i = 0; i < contours.size(); i++){
+				for(int j = 0; j < contours[i].size(); j++){
+					apoint.x = contours[i][j].x;
+					apoint.y = contours[i][j].y;
+					apoint.label = labelCounter;
+					labelCounter++;
+					temp1.push_back(apoint);
+				}
+			}
+			points.push_back(temp1);
+		} else {
+			for(int i = 0; i < contours.size(); i++){
+				for(int j = 0; j < contours[i].size(); j++){
+					apoint.x = contours[i][j].x;
+					apoint.y = contours[i][j].y;
+					apoint.label = -1;
+					temp2.push_back(apoint);
+				}
+			}
+	
+			for(int i = 0; i < temp2.size(); i++){
+				location[0] = temp2[i].x;
+				location[1] = temp2[i].y;
+				it.SetLocation(location);
+				for(int j = 0; j < 9; j++){
+					if(it.GetPixel(j) != 0){
+						for(int k = 0; k < temp1.size(); k++){
+							if(temp1[k].x == it.GetIndex(j)[0] && temp1[k].y == it.GetIndex(j)[1]){
+								temp2[i].label = temp1[k].label;
+							}
+						}
+					} else {
+						zeroCounter++;
+					}
+				}
+				if(zeroCounter == 9){
+					temp2[i].label = labelCounter;
+					labelCounter++;
+					zeroCounter = 0;
+				}
+			}
+
+			points.push_back(temp2);
+
+			temp1.clear();
+			temp1.resize(temp2.size());
+			memcpy(&temp1[0], &temp2[0], temp2.size() * sizeof(APoint));
+			temp2.clear();
+		}
+
+		//Z轴小于3的label删除
+		
+		ImageType2D::Pointer itkDrawing;
+		try{
+			itkDrawing=itk::OpenCVImageBridge::CVMatToITKImage< ImageType2D >( drawing );
+		} catch (itk::ExceptionObject &excp){
+			std::cerr << "Exception: CVMatToITKImage failure !" << std::endl;
+			std::cerr << excp << std::endl;
+			return EXIT_FAILURE;
+		}
+		joinSeries->PushBackInput( itkDrawing );
+	}
+
+	try{
+		joinSeries->Update();
+	} catch (itk::ExceptionObject &excp){
+		std::cerr << "ExceptionObject: joinSeries->Update() caught !" << std::endl;
+		std::cerr << excp << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	typedef itk::ImageFileWriter<ImageType3D> WriterType;
+	WriterType::Pointer writer = WriterType::New();
+	writer->SetFileName( argv[2] );
+	writer->SetInput( joinSeries->GetOutput() );
+
+	try{
+		writer->Update();
+	} catch (itk::ExceptionObject &excp){
+		std::cerr << "ExceptionObject caught !" << std::endl;
+		std::cerr << excp << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
