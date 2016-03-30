1f5dbba8f6a29e3c281ce2e79efa64e3308ec656
bowring <bowringchan@gmail.com>
2016-03-30 15:16:36 +0800

    completed

--- LDA/LDA.cpp
+++ LDA/LDA.cpp
@@ -22,8 +22,8 @@ using namespace std;
 //type:CV_64FC1
 //clacEivectorAndCenter(训练集，训练集每个元素的数据类型，标签，特征向量，投影中心，训练集的size);
 int calcEivectorAndCenter(vector<double> data,int type,vector<int>& labels,Mat& eivector,vector<Mat>& cluster,int sizeA,int sizeB){
-	Mat mat=Mat(sizeA,sizeB,CV_64FC1,&data[0],sizeB);//一维数组转二维mat
-	
+	Mat mat=Mat(sizeA,sizeB,CV_64FC1,&data[0]);//一维数组转二维mat
+	cout << "M = "<< endl << " "  << mat << endl << endl;
 	LDA lda=LDA(mat,labels);
 	eivector=lda.eigenvectors().clone();
 	cout<<"The eigenvector is:"<<endl;
@@ -93,11 +93,11 @@ int classDetermination(vector<double> data,int type,Mat& eivector,vector<int>& c
 
 	for(int i=0;i<sizeA;i++){
 		result[i]=Mat::zeros(1,1,type);
-		Mat dataArray=Mat(1,sizeB,CV_64FC1,&data[i*sizeB]);
-//		for(int j=0;j<2;j++)
-//			printf("dataArry[%d]=%f",j,dataArray.at<double>(j));
+		Mat dataArray=Mat(1,sizeB,CV_64FC1,&(data[i*sizeB]));
+		/*for(int j=0;j<10;j++)
+			printf("dataArry[%d]=%f|%f",j,dataArray.at<double>(j),data[i*sizeB+j]);*/
 		multiply(eivector.t(),dataArray,result[i]);
-//		printf("result[%d]=%f\n",i,result[i].at<double>(0));
+		printf("result[%d]=%f\n",i,result[i].at<double>(0));
 	}
 
 
@@ -122,10 +122,10 @@ int main(){
 	boost::archive::text_iarchive ia(file);
 	std::vector<double> features;
 	ia & BOOST_SERIALIZATION_NVP(features);
-	//导入结束
+	////导入结束
 	//for(int i=0;i<features.size();i++){
-	//	for(int j=0;j<features[i].size();j++)
-	//	std::cout<<features[i][j]<<std::endl;
+	//	
+	//	std::cout<<features[i]<<std::endl;
 	//}
     
 
@@ -135,9 +135,9 @@ int main(){
 	
 	//labels
 	vector<int>labels;
-	for(int i=0;i<2;i++)
+	for(int i=0;i<6;i++)
 	{
-		if(i<1)
+		if(i<3)
 		{
 			labels.push_back(0);
 		}
@@ -147,10 +147,10 @@ int main(){
 		}
 	}
 
-	int sizeA=features[features.size()-1];
-	int sizeB=features[features.size()-2];
-	features.erase(features.end());
-	features.erase(features.end());
+	int sizeA=features[features.size()-2];
+	int sizeB=features[features.size()-1];
+	features.erase(features.begin()+(features.size()-2));
+	features.erase(features.begin()+(features.size()-1));
 
 	Mat eivector;
 	vector<Mat> cluster(features.size());
@@ -158,21 +158,33 @@ int main(){
 	calcEivectorAndCenter(features,type,labels,eivector,cluster,sizeA,sizeB);
 	//calcEivectorAndCenter(sampledata,type,labels,eivector,cluster,sizeA,sizeB);
 	
+///
+	///测试，人造的数据，人造的数字，
+	///人造了6个，前3个所有值都是1 后三个所有值都是10
+	///
+	///
+	///
+
 	
-	vector<int> classBelongings(2);
+	vector<int> classBelongings(6);
 
 
-	vector<double> vTestData(20);
-	for(int i=0;i<20;i++){
-		if(i<10){
-			vTestData.push_back(0);
+	vector<double> vTestData;
+	for(int i=0;i<60;i++){
+		if(i<30){
+			vTestData.push_back(1);
 		}else{
 			vTestData.push_back(10);
 		}
 	}
 
-	classDetermination(vTestData,type,eivector,classBelongings,cluster,2,sizeB);
-	for(int i=0;i<2;i++){
+	//printf("\nvTestData:\n");
+	//for(int i=0;i<60;i++){
+	//	printf("%f",vTestData[i]);
+	//}
+
+	classDetermination(vTestData,type,eivector,classBelongings,cluster,6,sizeB);
+	for(int i=0;i<6;i++){
 		printf("|%d\t",classBelongings[i]);
 	}
 	return 0;
