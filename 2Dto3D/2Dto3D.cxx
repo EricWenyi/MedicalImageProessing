1508678881eda09db7b4b80e5d4baf9fbc3ec3c4
LuJia <LuJia@UESTC.org>
2016-04-14 21:47:20 +0800

    trim the code

--- 2Dto3D/2Dto3D.cxx
+++ 2Dto3D/2Dto3D.cxx
@@ -4,12 +4,7 @@
 #include "itkImageSeriesWriter.h"
 
 #include "itkThresholdImageFilter.h"
-#include "itkImage.h"
 #include "itkCurvatureFlowImageFilter.h"
-
-#include <vector>
-#include "itksys/SystemTools.hxx"
-#include "itkCastImageFilter.h"
 
 #include "itkImageSliceConstIteratorWithIndex.h"
 #include "itkExtractImageFilter.h"
@@ -17,20 +12,13 @@
 
 int main( int argc, char* argv[] ){
 	if( argc < 4 ){
-		std::cerr << "Usage: " << argv[0] << " DicomDirectory OutputDicomPath&Name lowerThreshold" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " dicomDirectory lowerThreshold originFile thresholdFile" << std::endl;
 		return EXIT_FAILURE;
     }
 
-	typedef signed short PixelType;
-	const unsigned int Dimension = 3;
-
-	typedef itk::Image< PixelType, Dimension > ImageType;
-	typedef itk::ImageSeriesReader< ImageType > ReaderType;
-
-	const unsigned int OutputDimension = 2;
-
-	typedef float OutputPixelType;//此处原为signed short，应设置为float类型，下文CurvatureFlowImageFilter所要求，否则将会弹窗报错至失去响应
-	typedef itk::Image< OutputPixelType, OutputDimension > OutputImageType;
+	typedef itk::Image< signed short, 3 > InputImageType;
+	typedef itk::ImageSeriesReader< InputImageType > ReaderType;
+	typedef itk::Image< float, 2 > OutputImageType;
 
 	typedef itk::GDCMImageIO ImageIOType;
 	typedef itk::GDCMSeriesFileNames NamesGeneratorType;
@@ -43,13 +31,13 @@ int main( int argc, char* argv[] ){
 	std::vector< std::string > tmp_filenames = filenames;
 	const unsigned int numberOfFileNames = filenames.size();
 
-    int j=numberOfFileNames;
-    for(int i=0; i< filenames.size(); i++){
-            tmp_filenames[i]=filenames[j-1];
-            j--;
+    int j = numberOfFileNames;
+    for(int i = 0; i < filenames.size(); i++){
+		tmp_filenames[i] = filenames[j-1];
+		j--;
     }
 
-	for(unsigned int fni = 0; fni < numberOfFileNames; ++fni){
+	for(unsigned int fni = 0; fni < numberOfFileNames; fni++){
 		std::cout << "filename # " << fni + 1 << " = ";
 		std::cout << tmp_filenames[fni] << std::endl;
     }
@@ -66,22 +54,25 @@ int main( int argc, char* argv[] ){
 		return EXIT_FAILURE;
 	}
 
-	ImageType::ConstPointer InputImage3D = reader->GetOutput();
-	typedef itk::ImageSliceConstIteratorWithIndex <ImageType> SliceIteratorType;
+	InputImageType::ConstPointer InputImage3D = reader->GetOutput();
+	typedef itk::ImageSliceConstIteratorWithIndex < InputImageType > SliceIteratorType;
 
 	SliceIteratorType inIterator( InputImage3D, InputImage3D->GetLargestPossibleRegion() );
 	inIterator.SetFirstDirection(0);
 	inIterator.SetSecondDirection(1);
 
-	typedef itk::ExtractImageFilter< ImageType, OutputImageType > ExtractFilterType;
-	typedef itk::JoinSeriesImageFilter< OutputImageType, ImageType > JoinSeriesFilterType;
+	typedef itk::ExtractImageFilter< InputImageType, OutputImageType > ExtractFilterType;
+	typedef itk::JoinSeriesImageFilter< OutputImageType, InputImageType > JoinSeriesFilterType;
 
-	JoinSeriesFilterType::Pointer joinSeries = JoinSeriesFilterType::New();
-	joinSeries->SetOrigin(InputImage3D->GetOrigin()[2]);
-	joinSeries->SetSpacing(InputImage3D->GetSpacing()[2]);
+	JoinSeriesFilterType::Pointer joinSeries1 = JoinSeriesFilterType::New();
+	JoinSeriesFilterType::Pointer joinSeries2 = JoinSeriesFilterType::New();
+	joinSeries1->SetOrigin(InputImage3D->GetOrigin()[2]);
+	joinSeries1->SetSpacing(InputImage3D->GetSpacing()[2]);
+	joinSeries2->SetOrigin(InputImage3D->GetOrigin()[2]);
+	joinSeries2->SetSpacing(InputImage3D->GetSpacing()[2]);
 
 	for(inIterator.GoToBegin(); !inIterator.IsAtEnd(); inIterator.NextSlice()){
-		ImageType::IndexType sliceIndex = inIterator.GetIndex();
+		InputImageType::IndexType sliceIndex = inIterator.GetIndex();
 
 		ExtractFilterType::InputImageRegionType::SizeType sliceSize = inIterator.GetRegion().GetSize();
 		sliceSize[2] = 0;
@@ -93,65 +84,79 @@ int main( int argc, char* argv[] ){
 		ExtractFilterType::Pointer inExtractor = ExtractFilterType::New();
 		inExtractor->SetExtractionRegion( sliceRegion );
 		inExtractor->InPlaceOn();
-		inExtractor->SetDirectionCollapseToSubmatrix();//此处原为无，如果不实现此方法（此方法用于设定方向坍塌的模式，看源文件里头的内容，虽没什么用，但还是强制需要设置），否则下面更新迭代器时将会捕获到异常
+		inExtractor->SetDirectionCollapseToSubmatrix();
 		inExtractor->SetInput( reader->GetOutput() );
 
 		try{
-			inExtractor->Update();//原此处报6010错误，添加try，catch后发现，上文有修复方法，于是将所有Update()方法都加了try，catch
+			inExtractor->Update();
 		} catch (itk::ExceptionObject &excp){
 			std::cerr << "ExceptionObject: inExtractor->Update() caught !" << std::endl;
 			std::cerr << excp << std::endl;
 			return EXIT_FAILURE;
 		}
 		
+		joinSeries1->PushBackInput( inExtractor->GetOutput() );
+
 		typedef itk::CurvatureFlowImageFilter< OutputImageType, OutputImageType > CurvatureFlowImageFilterType;
-  
 		CurvatureFlowImageFilterType::Pointer smoothing = CurvatureFlowImageFilterType::New();
-		smoothing->SetInput(inExtractor->GetOutput());
+		smoothing->SetInput( inExtractor->GetOutput() );
 
 		typedef itk::ThresholdImageFilter< OutputImageType > FilterType;
+		FilterType::Pointer thresholdFilter = FilterType::New();
 
-		FilterType::Pointer ThresholdFilter = FilterType::New();
-
+		const signed short lowerThreshold = atof( argv[2] );
 		
-		const PixelType lowerThreshold = atof( argv[3] );
-
-
-		ThresholdFilter->SetOutsideValue( -2048 );
-		ThresholdFilter->ThresholdBelow( lowerThreshold );
-  
-
-
-		ThresholdFilter->SetInput(smoothing->GetOutput());
+		thresholdFilter->SetOutsideValue( -2048 );
+		thresholdFilter->ThresholdBelow( lowerThreshold );
+		thresholdFilter->SetInput( smoothing->GetOutput() );
 		
 		try{
-			ThresholdFilter->Update();
+			thresholdFilter->Update();
 		} catch (itk::ExceptionObject &excp){
-			std::cerr << "ExceptionObject: connectedThreshold->Update() caught !" << std::endl;
+			std::cerr << "ExceptionObject: threshold->Update() caught !" << std::endl;
 			std::cerr << excp << std::endl;
 			return EXIT_FAILURE;
 		}
-		
-		joinSeries->PushBackInput( inExtractor->GetOutput() );
+
+		joinSeries2->PushBackInput( thresholdFilter->GetOutput() );
 	}
 
 	try{
-		joinSeries->Update();
+		joinSeries1->Update();
 	} catch (itk::ExceptionObject &excp){
-		std::cerr << "ExceptionObject: joinSeries->Update(); caught !" << std::endl;
+		std::cerr << "ExceptionObject: joinSeries1->Update() caught !" << std::endl;
 		std::cerr << excp << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	typedef itk::ImageFileWriter<ImageType> WriterType;
+	try{
+		joinSeries2->Update();
+	} catch (itk::ExceptionObject &excp){
+		std::cerr << "ExceptionObject: joinSeries1->Update() caught !" << std::endl;
+		std::cerr << excp << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	typedef itk::ImageFileWriter<InputImageType> WriterType;
 	WriterType::Pointer writer = WriterType::New();
-	writer->SetFileName( argv[2] );
-	writer->SetInput( joinSeries->GetOutput() );
+	writer->SetFileName( argv[3] );
+	writer->SetInput( joinSeries1->GetOutput() );
 
 	try{
 		writer->Update();
 	} catch (itk::ExceptionObject &excp){
-		std::cerr << "ExceptionObject caught !" << std::endl;
+		std::cerr << "ExceptionObject: writer->Update() caught !" << std::endl;
+		std::cerr << excp << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	writer->SetFileName( argv[4] );
+	writer->SetInput( joinSeries2->GetOutput() );
+
+	try{
+		writer->Update();
+	} catch (itk::ExceptionObject &excp){
+		std::cerr << "ExceptionObject: writer->Update() caught !" << std::endl;
 		std::cerr << excp << std::endl;
 		return EXIT_FAILURE;
 	}
