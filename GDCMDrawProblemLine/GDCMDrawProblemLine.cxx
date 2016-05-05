a511c9cfc2b5ddf0d9eeaf488719b3bac5c3e9db
LuJia <LuJia@UESTC.org>
2016-05-05 13:34:27 +0800

    fix bugs

--- GDCMDrawProblemLine/GDCMDrawProblemLine.cxx
+++ GDCMDrawProblemLine/GDCMDrawProblemLine.cxx
@@ -1,5 +1,4 @@
 #include "itkGDCMImageIO.h"
-#include "itkGDCMSeriesFileNames.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 
@@ -7,18 +6,34 @@
 #include "itkExtractImageFilter.h"
 #include "itkJoinseriesImageFilter.h"
 
-#include "itkCurvatureAnisotropicDiffusionImageFilter.h"
-#include "itkGradientMagnitudeRecursiveGaussianImageFilter.h"
-#include "itkSigmoidImageFilter.h"
-#include "itkFastMarchingImageFilter.h"
-#include "itkBinaryThresholdImageFilter.h"
-
-#include "itkRescaleIntensityImageFilter.h"
-
 #include "itkOpenCVImageBridge.h"
 
-#include "cv.h"
-#include <math.h>
+struct APoint{
+	int contour;
+	int n;
+	int x;
+	int y;
+	bool status;
+};
+
+cv::vector<cv::vector<cv::Point>> contours;
+cv::Vector<APoint> repairedByStatus;
+
+int repairContour(cv::Vector<APoint> &repairedByStatus,int i);
+
+int findStatusBegin(cv::Vector<APoint> &repairedByStatus,int i);
+
+int findStatusEnd(cv::Vector<APoint> &repairedByStatus,int i);
+
+int findNewContourBegin(cv::Vector<APoint> &repairedByStatus,int i);
+
+void delAndDrawLine(cv::Vector<APoint> &repairedByStatus,int i,int statusBegin,int statusEnd);
+
+void AddNewNode(int possition,int i,cv::Point &newNode);
+
+void NodeDelete(int possition,int i);
+
+int repairInsideContour(int i);
 
 int main( int argc, char* argv[] ){
 	if( argc < 3 ){
@@ -65,7 +80,7 @@ int main( int argc, char* argv[] ){
 	int tempInCounter = 1;
 	for( inIterator.GoToBegin(); !inIterator.IsAtEnd(); inIterator.NextSlice() ){
 		ImageType3D::IndexType sliceIndex = inIterator.GetIndex();
-		printf( "Slice Index --- %d ---", tempInCounter++ );
+		printf( "Slice Index --- %d ---\n", tempInCounter++ );
 		ExtractFilterType::InputImageRegionType::SizeType sliceSize = inIterator.GetRegion().GetSize();
 		sliceSize[2] = 0;
 
@@ -87,22 +102,20 @@ int main( int argc, char* argv[] ){
 			return EXIT_FAILURE;
 		}
 
-		//引入Opencv
 		using namespace cv;
+
+		cv::vector<cv::vector<cv::Point>> tempContours;
+		contours=tempContours;
+
+		cv::Vector<APoint> tempRepairedByStatus;
+		repairedByStatus=tempRepairedByStatus;
+
 		Mat img = itk::OpenCVImageBridge::ITKImageToCVMat< ImageType2D >( inExtractor->GetOutput() );
-		vector<vector<Point>> contours;
+
 		vector<Vec4i> hierarchy;
-		RNG rng( 12345 );
-		findContours( img, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE, Point(0, 0) );//注意参数，之前吃大亏了
+		findContours( img, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE, Point(0, 0) );
+		printf("End of findcontours\n");
 		
-		struct APoint{
-			int contour;
-			int n;
-			int x;
-			int y;
-			bool status;
-		};
-		Vector<APoint> repairedByStatus;
 		for (int i = 0; i< contours.size(); i++){
 			APoint apoint;
 			for(int q = 0; q < contours[i].size(); q++ ){
@@ -113,80 +126,94 @@ int main( int argc, char* argv[] ){
 				apoint.status = false;
 				repairedByStatus.push_back( apoint );
 			}
+
 			printf( "Enter %d/%d of contours\n", i + 1, contours.size() );
+
 			float *disClockWise;
 			float **disPixel;
 			disClockWise = (float *)malloc( sizeof(float) * contours[i].size() * contours[i].size() );
 			disPixel = (float **)malloc( sizeof(float) * contours[i].size() );
+
 			for( int a = 0; a < contours[i].size(); a++ ){
 				disPixel[a] = &disClockWise[a * contours[i].size()];
 			}
+
 			float *disCounterClockWise;
 			float **disCounterPixel;
 			disCounterClockWise = (float *)malloc( sizeof(float) * contours[i].size() * contours[i].size() );
 			disCounterPixel = (float **)malloc( sizeof(float) * contours[i].size() );
+
 			for( int a = 0; a < contours[i].size(); a++ ){
 				disCounterPixel[a] = &disCounterClockWise[a * contours[i].size()];
 			}
+
 			for( int j = 0; j < contours[i].size(); j++ ){
 				for( int k = 0; k < contours[i].size(); k++ ){
 					disPixel[j][k] = 0;
 					disCounterPixel[j][k] = 0;
 				}
 			}
+
 			for( int j = 0; j < contours[i].size(); j++ ){
 				for( int k = 0; k < contours[i].size(); k++ ){
 					if( j < k ){
 						for( int m = j; m < k ; m++ ){
-							if( (contours[i][m+1].x != contours[i][m].x)&&(contours[i][m+1].y != contours[i][m].y) ){ 
+							if( (contours[i][m + 1].x != contours[i][m].x) && (contours[i][m + 1].y != contours[i][m].y) ){ 
 								disPixel[j][k] += 1.4142135f; 
-							}else{
+							} else {
 								disPixel[j][k] += 1.0f;
 							}
 						}
 						for( int m = 0; m < j; m++ ){
-							if( (contours[i][m+1].x != contours[i][m].x)&&(contours[i][m+1].y != contours[i][m].y) ){
+							if( (contours[i][m + 1].x != contours[i][m].x) && (contours[i][m + 1].y != contours[i][m].y) ){
 								disCounterPixel[j][k] += 1.4142135f;
-							}else{
+							} else {
 								disCounterPixel[j][k] += 1.0f;
 							}
 						}
 						for( int m = k; m < contours[i].size() - 1; m++ ){
-							if( (contours[i][m+1].x != contours[i][m].x)&&(contours[i][m+1].y != contours[i][m].y) ){
+							if( (contours[i][m + 1].x != contours[i][m].x) && (contours[i][m + 1].y != contours[i][m].y) ){
 								disCounterPixel[j][k] += 1.4142135f; 
-							}else{
+							} else {
 								disCounterPixel[j][k] += 1.0f;
 							}
 						}
-						if( (contours[i][contours[i].size()-1].x != contours[i][0].x)&&(contours[i][contours[i].size()-1].y != contours[i][0].y) ){
+						if( (contours[i][contours[i].size() - 1].x != contours[i][0].x) && (contours[i][contours[i].size() - 1].y != contours[i][0].y) ){
 							disCounterPixel[j][k] += 1.4142135f;
-						}else{
+						} else {
 							disCounterPixel[j][k] += 1.0f;
 						}
-					}else if( j > k ){
+					} else if ( j > k ){
 						for( int m = j; m < contours[i].size(); m++ ){
 							disPixel[j][k] = disCounterPixel[k][j]; 
 							disCounterPixel[j][k] = disPixel[k][j];
 						}
-					}else{
+					} else {
 						disPixel[j][k] = 0;
 						disCounterPixel[j][k] = 0;
 					}
 				}
 			}
-			printf( "i:%d distance calc OK\n", i + 1 );
+
+			int Points_Ahead = 0;
+			for(int round_ahead = 0; round_ahead < i; round_ahead++){
+				Points_Ahead += contours[round_ahead].size();
+			}
+
+			printf( "%d Distance calculation OK\n", i + 1 );
+
 			for( int j = 0; j < contours[i].size(); j++ ){
 				for( int k = 0; k < contours[i].size(); k++ ){ 
 					if( disPixel[j][k] < disCounterPixel[j][k] ){
-						float disOfjk = (float)sqrt( (double)((contours[i][j].x - contours[i][k].x) * (contours[i][j].x - contours[i][k].x) + (contours[i][j].y - contours[i][k].y) * (contours[i][j].y - contours[i][k].y)) );
-						if( disPixel[j][k] / disOfjk > 1.5 ){		
+						float disOfJAndK = (float)sqrt( (double)((contours[i][j].x - contours[i][k].x) * (contours[i][j].x - contours[i][k].x) + (contours[i][j].y - contours[i][k].y) * (contours[i][j].y - contours[i][k].y)) );
+						if( disPixel[j][k] / disOfJAndK > 8 ){		
 							if( k > j ){
 								for( int p = j; p <= k; p++ ){
-									repairedByStatus[p].status = true;
+									repairedByStatus[p + Points_Ahead].status = true;
 								}
-							}else{
+							} else {
 								for( int p = j; p <= k + contours[i].size(); p++ ){
-									repairedByStatus[p % contours[i].size()].status = true;
+									repairedByStatus[(p % contours[i].size()) + Points_Ahead].status = true;
 								}
 							}
 						}
@@ -196,25 +223,21 @@ int main( int argc, char* argv[] ){
 			printf( "%d Detecting is OK, enter repair...\n", tempInCounter - 1 );
 		}
 
-		//TODO replace
+		printf("End of detecting\n");
 
-		// Draw contours
-		printf( "drawing\n" );
+		repairContour(repairedByStatus, 0);
+
+		printf("End of repairing\n");
+
+		printf( "Drawing\n" );
 		Mat drawing = Mat::zeros( img.size(), CV_8UC1 );
-		/*
+
 		for( int i = 0; i < contours.size(); i++ ){
-			Scalar color = Scalar( rng.uniform( 0, 255 ) );
-			drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, Point(0, 0) );//注意参数，之前吃大亏了
+			drawContours( drawing, contours, i, Scalar( 255 ), 1, 8, hierarchy, 0, Point(0, 0) );
 		}
-		*/
-		for( int i = 0; i < repairedByStatus.size(); i++ ){
-			if( repairedByStatus[i].status ){
-				drawing.at<uchar>( repairedByStatus[i].y, repairedByStatus[i].x ) = 128;
-			} else {
-				drawing.at<uchar>( repairedByStatus[i].y, repairedByStatus[i].x ) = 255;
-			}
-		}
+		
 		ImageType2D::Pointer itkDrawing;
+
 		try{
 			itkDrawing=itk::OpenCVImageBridge::CVMatToITKImage< ImageType2D >( drawing );
 		} catch (itk::ExceptionObject &excp){
@@ -222,7 +245,7 @@ int main( int argc, char* argv[] ){
 			std::cerr << excp << std::endl;
 			return EXIT_FAILURE;
 		}
-		printf( "%d ITK Drawing is OK\n", tempInCounter - 1 );
+
 		joinSeries->PushBackInput( itkDrawing );
 	}
 
@@ -247,4 +270,148 @@ int main( int argc, char* argv[] ){
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
+}
+
+int repairContour(cv::Vector<APoint> &repairedByStatus,int i){
+	int insideReturn = repairInsideContour(i);
+	if(insideReturn == 0){
+		return 0;
+	}
+	int newContourBeginI = findNewContourBegin(repairedByStatus,i);
+	if(newContourBeginI == -1){
+		return 0;
+	} else {
+		return repairContour(repairedByStatus,newContourBeginI);
+	}
+}
+
+int repairInsideContour(int i){
+	int statusBegin = 0;
+	printf("Reparing %d/%d repairedByStatus",i,repairedByStatus.size());
+	statusBegin = findStatusBegin(repairedByStatus,i);
+	if(statusBegin == -1){
+		int newContourBeginI = findNewContourBegin(repairedByStatus,i);
+		if(newContourBeginI == -1){
+			return 0;
+		} else {
+			return 1;
+		}
+	} else if (statusBegin == -2){
+		return 0;
+	}
+	int statusEnd = 0;
+	statusEnd = findStatusEnd(repairedByStatus,statusBegin);
+	if(statusEnd == -1){
+		return 0;
+	}
+	delAndDrawLine(repairedByStatus,repairedByStatus[i].contour,statusBegin,statusEnd);
+	if(statusEnd < repairedByStatus.size() && repairedByStatus[statusEnd].contour == repairedByStatus[statusEnd + 1].contour){
+		repairInsideContour(statusEnd + 1);
+	}
+	return 1;
+}
+
+
+int findStatusBegin(cv::Vector<APoint> &repairedByStatus,int i){
+	int currentContour = repairedByStatus[i].contour;
+	while(repairedByStatus[i].contour == currentContour && (!repairedByStatus[i].status) && i < repairedByStatus.size() - 1){
+		i++;
+	}
+	if(repairedByStatus[i].contour != currentContour){
+		return -1;
+	} else if (i == repairedByStatus.size() - 1){
+		return -2;
+	} else {
+		return i;
+	}
+}
+
+int findStatusEnd(cv::Vector<APoint> &repairedByStatus,int i){
+	int currentContour = repairedByStatus[i].contour;
+	while(i < repairedByStatus.size() - 1 && repairedByStatus[i].contour == currentContour && (repairedByStatus[i].status)){
+		i++;
+	}
+
+	if(i == repairedByStatus.size() - 1){
+		return -1;
+	} else {
+		return i - 1;
+	}
+}
+
+int findNewContourBegin(cv::Vector<APoint> &repairedByStatus,int i){
+	int currentContour = repairedByStatus[i].contour;
+	while(i < repairedByStatus.size() && repairedByStatus[i].contour == currentContour){
+		i++;
+	}
+	if(i < repairedByStatus.size()){
+		return i;
+	} else {
+		return -1;
+	}
+}
+
+void delAndDrawLine(cv::Vector<APoint> &repairedByStatus,int i,int statusBegin,int statusEnd){
+	int currentNode = 0;
+	while((contours[i][currentNode].x != repairedByStatus[statusBegin].x) || (contours[i][currentNode].y != repairedByStatus[statusBegin].y)){
+		currentNode++;
+	}
+	while((contours[i][currentNode].x != repairedByStatus[statusEnd].x) || (contours[i][currentNode].y != repairedByStatus[statusEnd].y)){
+		NodeDelete(currentNode,i);
+	}
+	int Rx = repairedByStatus[statusEnd].x - repairedByStatus[statusBegin].x;
+	int Ry = repairedByStatus[statusEnd].y - repairedByStatus[statusBegin].y;
+	printf("=============%d,%d===(%d,%d)(%d,%d)=======",Rx,Ry,repairedByStatus[statusBegin].x,repairedByStatus[statusBegin].y,repairedByStatus[statusEnd].x,repairedByStatus[statusEnd].y);
+	//assert the node of statusEnd is known, then we have four possible related possition cases.
+	if(Rx > 0 && Ry > 0){
+		for(int x = 1; x <= Rx; x++){
+			//first one, normal one.
+			int y = ((double)Ry) / ((double)Rx) * x + repairedByStatus[statusBegin].y;
+			cv::Point aPoint;
+			aPoint.x = x + repairedByStatus[statusBegin].x;
+			aPoint.y = y;
+			AddNewNode(currentNode,i,aPoint);
+		}
+	}
+
+	if(Rx < 0 && Ry > 0){
+		for(int x = -1; x >= Rx; x--){
+			//first one, normal one.
+			int y = ((double)Ry) / ((double)Rx) * x + repairedByStatus[statusBegin].y;
+			cv::Point aPoint;
+			aPoint.x = x + repairedByStatus[statusBegin].x;
+			aPoint.y = y;
+			AddNewNode(currentNode,i,aPoint);
+		}
+	}
+	
+	if(Rx < 0 && Ry < 0){
+		for(int x = -1; x >= Rx; x--){
+			//first one, normal one.
+			int y = ((double)Ry) / ((double)Rx) * x + repairedByStatus[statusBegin].y;
+			cv::Point aPoint;
+			aPoint.x = x + repairedByStatus[statusBegin].x;
+			aPoint.y = y;
+			AddNewNode(currentNode,i,aPoint);
+		}
+	}
+		
+	if(Rx > 0 && Ry < 0){
+		for(int x=1;x<=Rx;x++){
+			//first one, normal one.
+			int y = ((double)Ry) / ((double)Rx) * x + repairedByStatus[statusBegin].y;
+			cv::Point aPoint;
+			aPoint.x = x + repairedByStatus[statusBegin].x;
+			aPoint.y = y;
+			AddNewNode(currentNode,i,aPoint);
+		}
+	}
+}
+
+void AddNewNode(int possition, int i, cv::Point &newNode){
+	contours[i].insert(contours[i].begin() + possition, newNode);
+}
+
+void NodeDelete(int possition, int i){
+	contours[i].erase(contours[i].begin() + possition);
 }
