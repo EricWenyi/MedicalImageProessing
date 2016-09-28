4e32f3a6b6c2020cd4a4861b02426a04aab4aea9
LuJia <LuJia@UESTC.org>
2016-09-28 09:17:12 +0800

    fix bugs

--- MATLAB/CalcBlob.m
+++ MATLAB/CalcBlob.m
@@ -22,7 +22,7 @@ end
 FBlob = zeros(1, numel(Lambda1));
 
 for n = 1:numel(Lambda1)
-	if Lambda3(n) <= Lambda2(n) && Lambda2(n) < 0 && Lambda2(n) < Lambda1(n)
+	if Lambda3(n) <= Lambda2(n) && Lambda2(n) <= Lambda1(n) && Lambda1(n) < 0
 		if Lambda2(n) ~= 0 || Lambda3(n) ~= 0
 			FBlob(n) = 4 * Lambda1(n) / Lambda3(n) / (1 + (Lambda1(n) / Lambda2(n))^2 + (Lambda2(n) / Lambda3(n))^2 + (Lambda1(n) / Lambda3(n))^2);
         end
