aeeaae575321a9017956dad94d22862d3e7e7a73
LuJia <LuJia@UESTC.org>
2016-10-05 12:22:41 +0800

    add sigma

--- MATLAB/JudgeNodule.m
+++ MATLAB/JudgeNodule.m
@@ -1,10 +1,14 @@
-function JudgeNodule(dcmFile)
+function JudgeNodule(dcmFile, sigma)
 
 % mex eig3volume.c
 
+if nargin < 2
+    sigma = 1;
+end
+
 i = dicomread(dcmFile);
 o = double(i(:,:,:));
-[Dxx, Dyy, Dzz, Dxy, Dxz, Dyz] = Hessian3D(o);
+[Dxx, Dyy, Dzz, Dxy, Dxz, Dyz] = Hessian3D(o, sigma);
 clear o;
 [Lambda1, Lambda2, Lambda3] = eig3volume(Dxx, Dyy, Dzz, Dxy, Dxz, Dyz);
 clear Dxx Dyy Dzz Dxy Dxz Dyz;
@@ -22,7 +26,7 @@ end
 FVessel = zeros(1, numel(Lambda1));
 
 for n = 1:numel(Lambda1)
-	if Lambda3(n) <= Lambda2(n) && Lambda2(n) < 0 && Lambda2(n) < Lambda1(n)
+	if Lambda3(n) <= Lambda2(n) && Lambda2(n) < 0 && Lambda2(n) <= Lambda1(n)
 		if Lambda2(n) ~= 0 || Lambda3(n) ~= 0
 			FVessel(n) = exp(-(Lambda1(n) / Lambda2(n))^0.5) * (2 * Lambda2(n) / Lambda3(n)) / (1 + (Lambda2(n) / Lambda3(n))^2);
         end
@@ -34,4 +38,4 @@ clear Lambda1 Lambda2 Lambda3;
 
 i(find(FBlob > 0.5 & FVessel > 0 & FVessel < 0.7)) = 2047;
 clear FBlob FVessel;
-dicomwrite(i,'mark.dcm');
+dicomwrite(i,'temp.dcm');
